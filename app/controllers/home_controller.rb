f26f93a3b18efdf50cd1bf4954fb6996e8614ca7
Breno <breno.nunesgalvao@protonmail.ch>
2019-06-15 16:34:58 -0300

    logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,15 @@ class HomeController < ApplicationController
 
   def auth_callback
     auth = request.env['omniauth.auth']
-    session[:current_user] = { :nickname => auth.info['nickname'],
-                               :image => auth.info['image'],
-                               :uid => auth.uid }
+    session[:current_user] = { 'nickname' => auth.info['nickname'],
+                               'image' => auth.info['image'],
+                               'uid' => auth.uid }
+    redirect_to root_url
+  end
+
+  def logout
+    reset_session
+
     redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post 'auth/steam/callback' => 'home#auth_callback'
 
+  get 'logout' => 'home#logout'
+
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
