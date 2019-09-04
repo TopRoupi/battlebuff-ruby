247f0715f1bc36e012a37d67c298bc1ca5735527
breno <breno.nunesgalvao@protonmail.ch>
2019-09-03 21:12:58 -0300

    docket tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_211415) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "players", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.string "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
