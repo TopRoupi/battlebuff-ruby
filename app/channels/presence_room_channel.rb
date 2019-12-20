4ab3cd230665bf047a8a3b53887588100060bd78
Breno Nunes <breno.nunesgalvao@gmail.com>
2019-12-19 23:43:08 -0300

    sets visible false when the last player exist the room

--- app/channels/presence_room_channel.rb
+++ app/channels/presence_room_channel.rb
@@ -1,29 +1,37 @@
 class PresenceRoomChannel < ApplicationCable::Channel
   def subscribed
-    alter_online_status(status: true, room_id: params[:room_id])
-    stream_from "presence_room_#{params[:room_id]}_channel"
-    broadcast_players_list(params[:room_id])
+    room = Room.find(params[:room_id])
+
+    alter_online_status(room, status: true)
+    stream_from "presence_room_#{room.id}_channel"
+    broadcast_players_list(room)
 
     p "#{current_player} is online"
   end
 
   def unsubscribed
-    alter_online_status(status: false, room_id: params[:room_id])
+    room = Room.find(params[:room_id])
+
+    alter_online_status(room, status: false)
     stop_all_streams
-    broadcast_players_list(params[:room_id])
+    broadcast_players_list(room)
+
+    if room.online_players.length == 0
+      room.visible = false
+      room.save!
+    end
 
     p "#{current_player} is offline"
   end
 
   private
 
-  def broadcast_players_list(room_id)
-    room = Room.find(room_id)
-    ActionCable.server.broadcast "presence_room_#{room_id}_channel", room.online_players
+  def broadcast_players_list(room)
+    ActionCable.server.broadcast "presence_room_#{room.id}_channel", room.online_players
   end
 
-  def alter_online_status(status: nil, room_id: nil)
+  def alter_online_status(room, status: nil)
     player = Player.find(current_player)
-    player.set_online_status_by_room(status, room_id)
+    player.set_online_status_by_room(status, room.id)
   end
 end
